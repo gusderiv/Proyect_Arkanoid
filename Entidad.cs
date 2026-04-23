e8a8ba25f6f92d7c7fa87370e581c989388d0da5
Gustavo <gustavoadelgadillorivera@gmail.com>
2026-04-23 17:21:29 +0200

    Se implemento Dibujo, Movimiento dibujar y borrar

---
+++ Entidad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyect_Arkanoid
+{
+    internal abstract class Entidad
+    {
+        protected int x;
+        protected int velocidadX;
+
+
+
+        public int X { get => x; set => x = value; }
+        public int VelocidadX { get => velocidadX; set => velocidadX = value; }
+
+        public Entidad(int x, int velocidad)
+        {
+
+        }
+
+        public abstract void Dibujar();
+
+        public abstract void Borrar();
+
+
+    }
+}
