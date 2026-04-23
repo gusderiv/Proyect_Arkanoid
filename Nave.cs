6a738be222167526a151ede2604fdc33fc4d4aef
Iker Rabadán Cifuentes <ikerrabcif07@gmail.com>
2026-04-23 08:32:49 +0200

    He creado la clase nave con sus respectivos métodos

---
+++ Nave.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyect_Arkanoid
+{
+    internal class Nave
+    {
+        public Nave()
+        {
+
+        }
+
+        public void MoverNave(ref int x)
+        {
+            if(Console.KeyAvailable)
+            {
+                if(Console.ReadKey().Key == ConsoleKey.LeftArrow && x > 2)
+                {
+                    x--;
+                }
+                else if(Console.ReadKey().Key == ConsoleKey.RightArrow && x < Console.WindowWidth-8)
+                {
+                    x++;
+                }
+            }
+        }
+
+        public void DibujarNave(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+
+            Console.Write("[======]");
+        }
+
+        public void BorrarNave(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+
+            Console.Write(" ");
+        }
+    }
+}
