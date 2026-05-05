d2ebe0d9db66dbc8ae878b86d8f2a53d31876ce4
BorjaClase <borjamartinez197@gmail.com>
2026-05-05 08:34:54 +0200

    Cambios en la clase Ladrillo alumno A semana 3

--- Ladrillo.cs
+++ Ladrillo.cs
@@ -4,25 +4,36 @@ using System.Text;
 
 namespace Proyect_Arkanoid
 {
-    internal class Ladrillo
+    internal class Ladrillo:ObjetoJuego
     {
         private int x;
         private int y;
+        bool destruido;
+        int resistencia;
         Random rd = new Random();
-
-        public Ladrillo()
-        {
-            
-        }
 
         public int X { get => x; set => x = value; }
         public int Y { get => y; set => y = value; }
+
+        public Ladrillo() : base(0, 0)
+        {
+
+        }
 
         public void DibujoLadrillo()
         {
             //Sin implementar. int ladrilloX = rd.Next(x, y);
             Console.SetCursorPosition(23, 4); 
             Console.Write($"[###]");
+        }
+
+        public void RecibirGolpe()
+        {
+            resistencia--;
+            if(resistencia <= 0)
+            {
+                destruido = true;
+            }
         }
 
 
